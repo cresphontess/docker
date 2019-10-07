663d3d925cac7db00916fba10129a9677fec8c11
Aykut Karagoz <aykut.karagoz@medyasoft.com.tr>
2019-10-07 16:15:27 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Come onn yes !!
+          Come onn mann!!
         </p>
         <a
           className="App-link"
